8bc96c671dc4d930fc51f7d926d41b8bdfd881d8
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2023-02-28 21:07:11 -0300

    up
    up

--- calculadora.c
+++ calculadora.c
@@ -11,7 +11,7 @@ setlocale(LC_ALL, "Portuguese");
 
 while (loop == 1)
 {
-
+system("cls");
 printf("*** CALCULADORA ***\n");
 printf("Escolha uma op.\n");
 printf("Digite 1 - para somar.\n 2 Digite 2 - para subtrair.\n 3 Digite 3 - para multiplicar.\n 4 Digite 4 - para dividir.\n 5 Sair");
@@ -28,10 +28,11 @@ switch (op){
         printf("soma: %f.\n", res);
     break;
          case 2:
-        soma= n1 - n2;
+        sub= n1 - n2;
         printf("sub: %f.\n", res);
+    break;
          case 3:
-        soma= n1 / n2;
+        mult= n1 * n2;
         if(n2==0){
             printf("Divisão por 0!\n");
         } else{
@@ -39,16 +40,22 @@ switch (op){
     break;
     }
          case 4:
-        soma= n1 * n2;
+        div= n1 / n2;
         printf("mult: %f.\n", res);
+    break;
+        case 5:
+        loop = 0;
+        res = 0;
+        printf("A calculadora sera encerrada!");
     break;
 
 default:
-        printf("op invalida!\n");
-    break;
-}
+       res = 0;
+       printf("Nenhuma operação valida %d\n", res);
+
+        }
+    }
     system("pause");
     return 0;
 
-    }
 }
